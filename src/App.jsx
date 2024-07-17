9e5ec389b16d72b0019faa8aa5f5fb2de6188358
Izanalgit <izanalvarez@gmail.com>
2024-07-17 18:02:32 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-const App = () => {
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes , Link } from "react-router-dom";
+import Home from './Home.jsx';
+import ItemDetailPage from './ItemDetailPage.jsx';
 
+const App = () => {
+  const [data, setData] = useState(null);
+  const urlApi = 'http://localhost:3000';
+
+  const fetchData = async () => {
+    try{
+      const response = await fetch(urlApi)
+      const respData = await response.json();
+
+      setData(respData);
+
+    }catch(err){
+      console.error('Error : ', err)
+    }
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
   return (
-  <></>      
+    <Router>
+      <div>
+        <nav>
+          <Link to='/'>Inicio</Link>
+        </nav>
+        {data === null 
+          ? <div>Cargajdo ...</div>
+          : <Routes>
+              <Route path="/" element={<Home data={data} />} />
+              {data.map((item)=>(
+                <Route key={item._id} path={`/${item._id}`} element={<ItemDetailPage item={item} />}  />
+              ))}
+            </Routes>
+        }
+      </div>
+    </Router>      
   )
 };
 
